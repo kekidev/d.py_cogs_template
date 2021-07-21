666a8bf481573e4fedf861287030a69b3582dd1c
keki <72121955+kekidev@users.noreply.github.com>
2021-07-21 11:44:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from discord.ext import commands
 import os
 from dotenv import dotenv_values
 
+# .env 파일 불러오기
 config = dotenv_values(".env")
 
 intents = discord.Intents.all()
 
 client = commands.Bot(command_prefix=config.get("PREFIX"), intents=intents)
-client.remove_command("help")
+client.remove_command("help") # help 명령어 지우기
 
+# cogs 안에 있는 모든 파이썬 파일 불러오기
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")  
